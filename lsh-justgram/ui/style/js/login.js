77b6e688e83edf0001dc18dc5363ee6bdcd7ed30
Lee Shinhee <shlee2227@gmail.com>
2022-08-04 10:15:14 +0900

    edit: 파일 날아가서 다시 시작..

---
+++ lsh-justgram/ui/style/js/login.js
@@ -0,0 +1,38 @@
+//변수
+const accountData = document.getElementById("account-data");
+const passwordData = document.getElementById("password-data");
+let isvaild = false;
+
+const incomingData = document.getElementsByTagName("input");
+
+const loginBtn = document.getElementById("loginBtn");
+
+//함수
+
+function checkVaildInfo() {
+  let a = accountData.value.length;
+  let p = passwordData.value.length;
+  if (a !== 0 && p !== 0) {
+    isvaild = true;
+  } else {
+    isvaild = false;
+  }
+  if (isvaild === true) {
+    loginBtn.style.backgroundColor = "#083CD5";
+    loginBtn.style.cursor = "pointer";
+    loginBtn.disabled = false;
+  } else {
+    loginBtn.style.backgroundColor = "#b9d9f9";
+    loginBtn.disabled = true;
+  }
+  console.log(loginBtn);
+}
+
+//이벤트
+
+incomingData[0].addEventListener("input", checkVaildInfo);
+incomingData[1].addEventListener("input", checkVaildInfo);
+
+loginBtn.addEventListener("click", () => {
+  console.log("클릭됨");
+});
